7190737f3c4a5dcce5312d7a0a7093af92e31be6
archergu <649601376@qq.com>
2024-03-29 15:59:48 +0800

    chore: move music to assets

--- src/components/postcard/index.tsx
+++ src/components/postcard/index.tsx
@@ -1,18 +1,18 @@
 import "./index.css";
 const Index: React.FC = () => {
-    return (
-        <div className="postcard">
-            <div className="content">
-                <div className="stamp" ><div className="christmas" /></div>
-                <h1 className="to">To: 顾若煊</h1>
+  return (
+    <div className="postcard">
+      <div className="content">
+        <div className="stamp" ><div className="christmas" /></div>
+        <h1 className="to">To: 我的孩子们</h1>
 
-                <p>宝贝女儿，</p>
-                <p>这是圣诞节礼物。</p>
-                <p>圣诞节快乐！希望你能快乐成长！</p>
-                <p className="from">爱你的爸爸</p>
-            </div>
-        </div>
-    )
+        <p>孩子们，</p>
+        <p>这是圣诞节礼物。</p>
+        <p>圣诞节快乐！希望你们能快乐成长！</p>
+        <p className="from">爱你们的爸爸</p>
+      </div>
+    </div>
+  )
 }
 
 export default Index;
