1efbd7ad3228647b7f117e67529967ad5e4de071
archergu <649601376@qq.com>
2024-03-29 16:08:48 +0800

    feat: support pwa

--- vite.config.ts
+++ vite.config.ts
@@ -9,53 +9,68 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    publicDir: join(__dirname, 'src/public'),
-    server: {
-        https: true
-    },
-    plugins: [
-        react(),
-        mkcert({
-            source: 'coding'
-        }),
-        AutoImport({
-            dts: join(__dirname, 'src/auto-imports.d.ts'),
-            resolvers: [
-                IconsResolver({
-                    prefix: 'Icon',
-                    extension: 'jsx'
-                })
-            ],
-        }),
-        Icons({
-            compiler: 'jsx',
-            jsx: 'react'
-        }),
-        VitePWA({
-            registerType: 'autoUpdate',
-            manifest: {
-                name: 'Xmas2022',
-                short_name: 'Xmas2022',
-                theme_color: '#000000',
-                icons: [
-                    {
-                        src: '/pwa-250x250.png',
-                        sizes: '250x250',
-                        type: 'image/png',
-                    },
-                    {
-                        src: '/pwa-300x300.png',
-                        sizes: '300x300',
-                        type: 'image/png',
-                    },
-                    {
-                        src: '/pwa-300x300.png',
-                        sizes: '300x300',
-                        type: 'image/png',
-                        purpose: 'maskable any',
-                    },
-                ],
+  publicDir: join(__dirname, 'src/public'),
+  server: {
+    https: true
+  },
+  plugins: [
+    react(),
+    mkcert({
+      source: 'coding'
+    }),
+    AutoImport({
+      dts: join(__dirname, 'src/auto-imports.d.ts'),
+      resolvers: [
+        IconsResolver({
+          prefix: 'Icon',
+          extension: 'jsx'
+        })
+      ],
+    }),
+    Icons({
+      compiler: 'jsx',
+      jsx: 'react'
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Xmas2022',
+        short_name: 'Xmas2022',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/300x300.png',
+            sizes: '300x300',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        disableDevLogs: true,
+        runtimeCaching: [
+          {
+            urlPattern: /(.*?)\.(png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps|ico|webp)/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache',
             },
-        }),
-    ]
+          },
+          {
+            urlPattern: /(.*?)\.(gltf|glb|hdr)/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'model-cache',
+            },
+          },
+          {
+            urlPattern: /(.*?)\.(mp3|wav|ogg)/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'music-cache',
+            },
+          },
+        ],
+      }
+    }),
+  ]
 })
