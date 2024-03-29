55b671981d85489d3984b0675f8fc7478bc1d3be
archergu <649601376@qq.com>
2024-03-29 16:26:42 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
 import { join } from 'path';
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
@@ -10,16 +9,9 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: join(__dirname, 'src/public'),
-  server: {
-    https: true
-  },
   plugins: [
     react(),
-    mkcert({
-      source: 'coding'
-    }),
     AutoImport({
-      dts: join(__dirname, 'src/auto-imports.d.ts'),
       resolvers: [
         IconsResolver({
           prefix: 'Icon',
